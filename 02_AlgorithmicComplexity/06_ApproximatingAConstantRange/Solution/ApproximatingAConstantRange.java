ee0a7001b4ff8b67c42739a9f92baae56dde9382
Việt Hoàng <daoleviethoang@gmail.com>
2023-12-04 17:36:17 +0700

    02 06

---
+++ 02_AlgorithmicComplexity/06_ApproximatingAConstantRange/Solution/ApproximatingAConstantRange.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ApproximatingAConstantRange {
+    public int solve(int n, int[] a) {
+        return 0;
+    }
+    public static void main(String[] args) {
+        ApproximatingAConstantRange solution = new ApproximatingAConstantRange();
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int[] a = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            a[i] = scanner.nextInt();
+        }
+        scanner.close();
+
+        System.out.print(solution.solve(n, a));
+    }
+}
