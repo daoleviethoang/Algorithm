bf249dfe122705f09887f7e6d92b57f0d511df47
Việt Hoàng <daoleviethoang@gmail.com>
2023-12-07 17:30:26 +0700

    03 07

--- 03_Sorting/07_BusinessTrip/Solution/BusinessTrip.java
+++ 03_Sorting/07_BusinessTrip/Solution/BusinessTrip.java
@@ -1,3 +1,35 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class BusinessTrip {
-    
+    public int solve(int k, int[] a) {
+
+        if (k == 0) return 0;
+
+        Arrays.sort(a);
+
+        int results = 0 ;
+        for (int i = 11; i >= 0; i--) {
+            results += a[i];
+            if (results >= k) {
+                return 12 - i;
+            }
+        }
+
+        return -1;
+    }
+    public static void main(String[] args) {
+        BusinessTrip solution = new BusinessTrip();
+        Scanner scanner = new Scanner(System.in);
+
+        int k = scanner.nextInt();
+        int[] a = new int[12];
+
+        for (int i = 0; i < 12; i++) {
+            a[i] = scanner.nextInt();
+        }
+        scanner.close();
+
+        System.out.print(solution.solve(k, a));
+    }
 }
