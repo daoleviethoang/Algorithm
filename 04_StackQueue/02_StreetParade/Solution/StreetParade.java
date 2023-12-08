671ac149027c18f483b4bd9d4753195cba7fc972
Việt Hoàng <daoleviethoang@gmail.com>
2023-12-08 16:15:45 +0700

    02 04

--- 04_StackQueue/02_StreetParade/Solution/StreetParade.java
+++ 04_StackQueue/02_StreetParade/Solution/StreetParade.java
@@ -1,3 +1,78 @@
+import java.util.Scanner;
+import java.util.Stack;
+
 public class StreetParade {
+
+    public String solve(int n, int[] cars) {
+
+        Stack<Integer> sideStreet = new Stack<>();
+        int lastLoveMobiles = 0;
+
+        for (int i = 0; i < n; i++) {
+            
+            if (cars[i] == 1) {
+                lastLoveMobiles = cars[i];
+                continue;
+            }
+
+            if (sideStreet.empty()) {
+                if (cars[i] - lastLoveMobiles > 1) {
+                    sideStreet.add(cars[i]);
+                } else {
+                    lastLoveMobiles = cars[i];
+                    continue;
+                }               
+            } else {
+                if (cars[i] > sideStreet.peek()) {
+                    if (lastLoveMobiles != 0) {
+                        if (cars[i] - lastLoveMobiles > 1) {
+                            while (!sideStreet.isEmpty()) {
+                                if (sideStreet.peek() - lastLoveMobiles > 1) break;
+                                lastLoveMobiles = sideStreet.peek();
+                                sideStreet.pop();
+                            }
+
+                            if (!sideStreet.isEmpty()) {
+                                if (cars[i] > sideStreet.peek()) {
+                                    return "no";
+                                }
+                            }
+                            sideStreet.add(cars[i]);
+                            continue;
+                        }
+                    } else {
+                        return "no";
+                    }
+                } else if (cars[i] < sideStreet.peek()) {
+                    if (cars[i] - lastLoveMobiles == 1) {
+                        lastLoveMobiles = cars[i];
+                        continue;
+                    } else {
+                        sideStreet.add(cars[i]);
+                    }
+                }
+            }
+        }
+        return "yes";
+    }
     
+    public static void main(String[] args) {
+        StreetParade solution = new StreetParade();
+        Scanner scanner = new Scanner(System.in);
+
+        int n;
+        int[] a;
+        while (true) {
+            n = scanner.nextInt();
+
+            if (n == 0) break;
+
+            a = new int [n];
+            for (int i = 0; i < n; i++) {
+                a[i] = scanner.nextInt();
+            }
+            System.out.println(solution.solve(n, a));
+        }
+        scanner.close();
+    }
 }
